01ee2cf9aeff5215b0bd508ea39e9013b46d57cd
Your Name <youremail@example.com>
2025-07-21 19:03:27 +0100

    Fix

--- vite-project/src/Component/Project/project.jsx
+++ vite-project/src/Component/Project/project.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './project.css';
-import Project1 from '/src/assets/church_website.png';
+import Project1 from '/src/assets/Church_website.png';
 import Project2 from '/src/assets/E_commerce.png';
-import Project3 from '/src/assets/investment_site.png';
+import Project3 from '/src/assets/Investment_site.png';
 
 
 const project = () => {
